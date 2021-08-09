1a072fee97c4170ffef8de6a5c932ff900310070
hakim <hakimchaabani6@gmail.com>
2021-08-09 11:07:30 +0200

    first commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import {Card, Button} from 'react-bootstrap'
+import '../App.css'
+import StarRatingComponent from 'react-star-rating-component';
+
+const MovieCard = ({movie: {title, description, posterURL, rating}}) => {
+
+    return (
+        <div className='movieCard'>            
+            <Card style={{ width: '16rem'}}>
+                <Card.Img style={{width:'15.7rem', margin:'0.5%'}} variant="top" src={posterURL} />
+                <Card.Body>
+                    <Card.Title className='cardTitle'>{title}</Card.Title>
+                    <Card.Text className='desc'>
+                    {description}
+                    </Card.Text>
+                    <StarRatingComponent 
+                       
+                        value={rating}
+                    />
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default MovieCard
